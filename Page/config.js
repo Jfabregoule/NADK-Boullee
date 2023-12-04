a4d5a6449a2212467ab6af0aca8071953a46281f
Julien <Jfabregoule@gaming.tech>
2023-12-04 12:11:08 +0100

    changes

---
+++ Page/config.js
@@ -0,0 +1,3 @@
+export const publicToken = "public_k5-77H4uXpW_JUR6";
+export const mainSceneUUID = "63f24671-7b35-486b-b1d6-14cf456e3199";
+export const characterControllerSceneUUID = "5ff87a05-ff17-44b4-a768-0142fd0f9d29";
