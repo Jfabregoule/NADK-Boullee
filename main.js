41b883de8bc1b0d7203705ddf5059dd0172c2c9d
EnzoNumber7 <ephilippe@gaming.tech>
2023-12-20 15:02:52 +0100

    last enigma

--- main.js
+++ main.js
@@ -826,6 +826,12 @@ async function ButtonEnigma(){
 	async function InitGrabbable(){
 		let cubes = await SDK3DVerse.engineAPI.findEntitiesByNames('cubeEntity');
 		grabbable.push(...cubes);
+		cubes = await SDK3DVerse.engineAPI.findEntitiesByNames('redCube');
+		grabbable.push(...cubes);
+		cubes = await SDK3DVerse.engineAPI.findEntitiesByNames('purpleCube');
+		grabbable.push(...cubes);
+		cubes = await SDK3DVerse.engineAPI.findEntitiesByNames('lightCube');
+		grabbable.push(...cubes);
 	}
 
 	InitGrabbable();
