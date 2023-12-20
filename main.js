1971a0cc6ea9e94a13a6ffeb85cdccf7b625563e
EnzoNumber7 <ephilippe@gaming.tech>
2023-12-20 14:48:26 +0100

    enigma

--- main.js
+++ main.js
@@ -293,6 +293,124 @@ async function Game(){
 	}
 	await checkColls();
 
+	/*
+---------------------------------------------------------------------------------------------
+|																							|
+|										Enigma												|
+|																							|
+---------------------------------------------------------------------------------------------
+*/
+async function InitEnigma(){
+	enigmaDetectors = [];
+	enigmaEntities = [];
+	let detector = (await SDK3DVerse.engineAPI.findEntitiesByNames('wallDetector'));
+	enigmaDetectors.push(...detector);
+	detector = (await SDK3DVerse.engineAPI.findEntitiesByNames('redDetector'));
+	enigmaDetectors.push(...detector);
+	detector = (await SDK3DVerse.engineAPI.findEntitiesByNames('purpleDetector'));
+	enigmaDetectors.push(...detector);
+	detector = (await SDK3DVerse.engineAPI.findEntitiesByNames('lightDetector'));
+	enigmaDetectors.push(...detector);
+
+	let item = (await SDK3DVerse.engineAPI.findEntitiesByNames('cubeEntity'));
+	enigmaEntities.push(...item);
+	item = (await SDK3DVerse.engineAPI.findEntitiesByNames('redCube'));
+	enigmaEntities.push(...item);
+	item = (await SDK3DVerse.engineAPI.findEntitiesByNames('purpleCube'));
+	enigmaEntities.push(...item);
+	item = (await SDK3DVerse.engineAPI.findEntitiesByNames('lightCube'));
+	enigmaEntities.push(...item);
+}
+InitEnigma()
+
+async function Enigma(entity, detector){
+	if (enigmaEntities.includes(entity) && enigmaDetectors.includes(detector)){
+		
+		if (entity.getName() == 'cubeEntity' && detector.getName() == 'wallDetector'){
+			wall.setVisibility(false);	
+			wall.detachComponent('physics_material');
+		}
+		if (entity.getName() == 'redCube' && detector.getName() == 'redDetector'){
+			red = true;
+		}
+		if (entity.getName() == 'purpleCube' && detector.getName() == 'purpleDetector'){
+			purple = true;
+		}
+		if (entity.getName() == 'lightCube' && detector.getName() == 'lightDetector'){
+			light = true;
+		}
+
+		if (red && purple && light){
+			wall2.setVisibility(false);	
+			wall2.detachComponent('physics_material');
+		}
+	}
+}
+
+document.addEventListener('keyup',(event)=>{
+	if(event.key == 'f'){
+		ButtonEnigma();
+	}
+})
+
+async function ButtonEnigma(){
+	if (JSON.stringify(code) != JSON.stringify(codeTry)){
+		const cameraTransform = camera.getTransform();
+
+		// dirVect
+		let directionVector = [
+			SDK3DVerse.engineAPI.cameraAPI.getActiveViewports()[0].getWorldMatrix()[8],   // X
+			SDK3DVerse.engineAPI.cameraAPI.getActiveViewports()[0].getWorldMatrix()[9],   // Y
+			SDK3DVerse.engineAPI.cameraAPI.getActiveViewports()[0].getWorldMatrix()[10]   // Z
+		]; 	
+		// Normalise le vecteur si nécessaire
+		const magnitude = Math.sqrt(
+			directionVector[0] ** 2 + directionVector[1] ** 2 + directionVector[2] ** 2
+		);
+		directionVector = [
+			-directionVector[0] / magnitude,
+			-directionVector[1] / magnitude,
+			-directionVector[2] / magnitude
+		];
+
+		const origin = [
+		cameraTransform.position[0] + directionVector[0], // Multiplie par la distance souhaitée
+		cameraTransform.position[1] + directionVector[1],
+		cameraTransform.position[2] + directionVector[2]
+		];
+
+		const rayLength = 1;
+		const filterFlags = SDK3DVerse.PhysicsQueryFilterFlag.dynamic_block | SDK3DVerse.PhysicsQueryFilterFlag.record_touches;
+		// Returns dynamic body (if the ray hit one) in block, and all static bodies encountered along the way in touches
+		const{ block, touches } = await SDK3DVerse.engineAPI.physicsRaycast(origin, directionVector, rayLength, filterFlags);
+		if (block != null )
+		{
+			if (block.entity.getComponent('tags')){
+				if (block.entity.getComponent('tags').value[0] == 'button'){
+					if (lastBtn != null){
+						let pos = lastBtn.getGlobalTransform().position;
+						lastBtn.setGlobalTransform({position: [pos[0] - 0.05, pos[1], pos[2]]});
+						lastBtn = null;
+					}
+					codeTry.push(block.entity.getComponent('tags').value[1])
+					let pos = block.entity.getGlobalTransform().position;
+					block.entity.setGlobalTransform({position : [pos[0] + 0.05, pos[1], pos[2]]});
+					lastBtn = block.entity;
+				}
+			}
+		}
+	}
+	if (JSON.stringify(code) == JSON.stringify(codeTry)){
+		codeTry = [];
+		codeInteract.setComponent('material_ref',{value : "cf7f45ff-014b-4c2c-90fa-1deb01a2a4bb"})
+	}
+	if (codeTry.length == 3 && JSON.stringify(code) != JSON.stringify(codeTry)){
+		codeTry = [];
+		codeInteract.setComponent('material_ref',{value : "5629a0e5-e272-4be1-82e1-c8d6cef9ae76"})
+	}
+	return false;
+}
+
 /*
 ---------------------------------------------------------------------------------------------
 |																							|
