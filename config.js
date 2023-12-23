60d22c7900b9b13173c7350187a16654b58287ac
Jfabregoule <Jfabregoule@gaming.tech>
2023-12-23 15:25:29 +0100

    changed char controller id

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const publicToken = "public_QVc-b3SZ8MGGqcbs";
 export const mainSceneUUID = "adcce05f-73cf-4d00-81af-a0bb4c8f8ab7";
-export const characterControllerSceneUUID = "6b31c1c6-bbb1-4051-a7de-c82cd774f1bc";
+export const characterControllerSceneUUID = "9d04a662-fa10-4f3d-89f5-dfbe50e0c396";
 export const objectMeshUUID = "aaecea96-f7dd-490c-80e3-7eaddf5c7cf2";
 export const mirrorSceneUUID = "304b2d8d-dc26-4c24-ae4f-20a8aa6fcab0";
 export const phantomMeshUUID = "9cd3541d-80fb-4dcb-bd15-65cc9d205e28";
