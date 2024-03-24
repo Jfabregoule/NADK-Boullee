f2589c2101a3a3019adedac8d54ce935b526a85e
EnzoNumber7 <117282665+EnzoNumber7@users.noreply.github.com>
2024-03-24 19:30:32 +0100

    merge from sorted to clean

--- main.js
+++ main.js
@@ -162,6 +162,27 @@ async function Game(){
 	|																							|
 	---------------------------------------------------------------------------------------------
 	*/
+	
+	let TeleporterIn = [];
+	let TeleporterOut = [];
+	let tp = (await SDK3DVerse.engineAPI.findEntitiesByNames('teleporterToRoom1'));
+	TeleporterIn.push(...tp);
+	tp = (await SDK3DVerse.engineAPI.findEntitiesByNames('teleporterToRoom2'));
+	TeleporterIn.push(...tp);
+	tp = (await SDK3DVerse.engineAPI.findEntitiesByNames('teleporterToRoom3'));
+	TeleporterIn.push(...tp);
+	tp = (await SDK3DVerse.engineAPI.findEntitiesByNames('teleporterToRoom4'));
+	TeleporterIn.push(...tp);
+
+
+	let tpOut = (await SDK3DVerse.engineAPI.findEntitiesByNames('teleporterOut1'));
+	TeleporterOut.push(...tpOut);
+	tpOut = (await SDK3DVerse.engineAPI.findEntitiesByNames('teleporterOut2'));
+	TeleporterOut.push(...tpOut);
+	tpOut = (await SDK3DVerse.engineAPI.findEntitiesByNames('teleporterOut3'));
+	TeleporterOut.push(...tpOut);
+	tpOut = (await SDK3DVerse.engineAPI.findEntitiesByNames('teleporterOut4'));
+	TeleporterOut.push(...tpOut);
 
 	let hasSeenCinematic = false;
 	let isShooting;
